203d333e6cbcef8ef255b0c8d2ebf188c3f53e4a
Valdas Rakutis <valdas@rakutis.lt>
2019-05-25 15:05:49 +0300

    Pass props into profile page from redux state

--- need-money/src/components/profile/Profile.js
+++ need-money/src/components/profile/Profile.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { connect } from "react-redux";
+import styles from "../style";
+import { Button } from "react-native-elements";
 
 class Profile extends React.Component {
   render() {
+    console.log(this.props);
+    const { user } = this.props;
     return (
       <View>
-        <Text>Hello world</Text>
+        <Text>{user.username}</Text>
+        <Text>{`Reitingas: ${user.rating}`}</Text>
+        <Button title="Paskolinti" />
+        <Button title="Pasiskolinti" />
       </View>
     );
   }
 }
 
-export default Profile;
+const mapStateToProps = state => {
+  return {
+    user: state.login.user
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(Profile);
