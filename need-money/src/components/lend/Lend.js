6b2032352399203c17e178e8a667aee7efa74a19
Valdas Rakutis <valdas@rakutis.lt>
2019-06-06 10:38:11 +0300

    Update Lend Cornfirm Dialog

--- need-money/src/components/lend/Lend.js
+++ need-money/src/components/lend/Lend.js
@@ -3,16 +3,12 @@ import { TouchableOpacity, FlatList, Text, View } from "react-native";
 import styles from "../style";
 import { connect } from "react-redux";
 import { lend } from "../../store/actions/actions";
-import Dialog, {
-  DialogTitle,
-  DialogContent,
-  DialogFooter,
-  DialogButton
-} from "react-native-popup-dialog";
+import LendConfirmDialog from "./LendConfirmDialog";
 
 class Lend extends React.Component {
   state = {
-    defaultAnimationDialog: false
+    showDialog: false,
+    dialogRequest: undefined
   };
   render() {
     const { requests, tryLend } = this.props;
@@ -25,67 +21,27 @@ class Lend extends React.Component {
     });
     return (
       <View style={styles.backgroundColorView}>
+        <LendConfirmDialog
+          visible={this.state.showDialog}
+          onDismiss={() => this.setState({ showDialog: false })}
+          onCancel={() => this.setState({ showDialog: false })}
+          onConfirm={() => {
+            tryLend(this.state.dialogRequest);
+            this.setState({ showDialog: false });
+          }}
+        />
         <FlatList
           data={data}
           renderItem={({ item }) => (
             <TouchableOpacity
               onPress={() => {
                 this.setState({
-                  defaultAnimationDialog: true
+                  showDialog: true,
+                  dialogRequest: item.request
                 });
-                // tryLend(item.request);
               }}
             >
               <Text style={styles.item}>{item.title}</Text>
-
-              <Dialog
-                onDismiss={() => {
-                  this.setState({ defaultAnimationDialog: false });
-                }}
-                width={0.9}
-                visible={this.state.defaultAnimationDialog}
-                rounded
-                actionsBordered
-                dialogTitle={
-                  <DialogTitle
-                    title="Confirmation"
-                    style={{
-                      backgroundColor: "#F7F7F8"
-                    }}
-                    hasTitleBar={false}
-                    align="left"
-                  />
-                }
-                footer={
-                  <DialogFooter>
-                    <DialogButton
-                      text="CANCEL"
-                      bordered
-                      onPress={() => {
-                        this.setState({ defaultAnimationDialog: false });
-                      }}
-                      key="button-1"
-                    />
-                    <DialogButton
-                      text="YES"
-                      bordered
-                      onPress={() => {
-                        this.setState({ defaultAnimationDialog: false });
-                        tryLend(item.request);
-                      }}
-                      key="button-2"
-                    />
-                  </DialogFooter>
-                }
-              >
-                <DialogContent
-                  style={{
-                    backgroundColor: "#F7F7F8"
-                  }}
-                >
-                  <Text>Are you sure you want to lend this sum?</Text>
-                </DialogContent>
-              </Dialog>
             </TouchableOpacity>
           )}
         />
--- need-money/src/components/lend/LendConfirmDialog.js
+++ need-money/src/components/lend/LendConfirmDialog.js
@@ -1,16 +1,54 @@
 import React from "react";
-import Dialog, { DialogTitle, DialogContent } from "react-native-popup-dialog";
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+  DialogFooter,
+  DialogButton
+} from "react-native-popup-dialog";
 import { Button } from "react-native-elements";
 
 class LendConfirmDialog extends React.Component {
   render() {
-    <Dialog>
-      <DialogTitle>Confirm</DialogTitle>
-      <DialogContent>
-        <Button
-          title="Cancel"
-          onPress={() => this.setState({ visible: false })}
+    const { onDismiss, visible, onCancel, onConfrim } = this.props;
+    <Dialog
+      onDismiss={onDismiss}
+      width={0.9}
+      visible={visible}
+      rounded
+      actionsBordered
+      dialogTitle={
+        <DialogTitle
+          title="Confirmation"
+          style={{
+            backgroundColor: "#F7F7F8"
+          }}
+          hasTitleBar={false}
+          align="left"
         />
+      }
+      footer={
+        <DialogFooter>
+          <DialogButton
+            text="CANCEL"
+            bordered
+            onPress={onCancel}
+            key="button-1"
+          />
+          <DialogButton
+            text="YES"
+            bordered
+            onPress={onConfrim}
+            key="button-2"
+          />
+        </DialogFooter>
+      }
+    >
+      <DialogContent
+        style={{
+          backgroundColor: "#F7F7F8"
+        }}
+      >
+        <Text>Are you sure you want to lend this sum?</Text>
       </DialogContent>
     </Dialog>;
   }
